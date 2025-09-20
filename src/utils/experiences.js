f3e50197a7c2db839de2b9168331f51a08d72bea
Hardik Srivastava <144905418+hardiksrivastavaa@users.noreply.github.com>
2025-09-20 22:57:50 +0530

    Update experiences.js

--- src/utils/experiences.js
+++ src/utils/experiences.js
@@ -2,8 +2,8 @@ const experiences = [
 
   {
     title: "Google Arcade Facilitator Program",
-    role: "Trainee",
-    duration: "Apr 2025 – Jun 2025 (3 months)",
+    role: "Cloud Trainee",
+    duration: "Apr. 2025 – Jun. 2025",
     type: "Hybrid",
     description: "Engaged in 420+ labs covering core and advanced Google Cloud Platform services, including Compute Engine, Cloud Functions, BigQuery, Kubernetes, IAM, and more. This Arcade Facilitator program offered extensive practical exposure to real-world cloud scenarios.",
     image: "/certificates/arcade.jpg",
@@ -11,9 +11,10 @@ const experiences = [
   },
 
   {
-    title: "Summer Intern – Softpro India Computer Technologies",
-    duration: "Jul 2024 – Sep 2024 (3 months)",
-    location: "Lucknow · On-site",
+    title: "Softpro India Computer Technologies",
+    role: "Summer Trainee",
+    duration: "Jul. 2024 – Sep. 2024",
+    location: "Lucknow",
     description: "Worked with Spring Boot, Core Java, and REST APIs during an internship focused on enterprise app development.",
     image: "/certificates/spi.jpg",
     borderColor: "border-orange-500"
